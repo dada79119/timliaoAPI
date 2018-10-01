c81d48b18b36c842bbe1f012ea044d07c744caf6
Blues_Li <dada79119@gmail.com>
2018-10-01 13:12:58 +1000

    fix structure - app_run

--- app_run.py
+++ app_run.py
@@ -5,6 +5,7 @@ import time
 import os
 import urllib.request
 import json
+from flask import Flask, render_template, request, redirect, session
 
 # ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -19,17 +20,6 @@ def srapy(url):
 
     return returnText
 
-# def srapy(url):
-#     for page_num in range (1, 11):
-#         payload = {'fid': 18, 'page': page_num}
-#         print(payload)
-#     return payload    
-#     re = requests.get(url = url, params=payload)
-#     returnText = BeautifulSoup(re.text, 'html.parser')
-
-#     return returnText
-
-
 def get_link(dom):
     articleLink=[]
     for article in dom.findAll("h2",{'class','subject'}):
@@ -39,65 +29,35 @@ def get_link(dom):
 
     return articleLink
 
-def download_pic(link):
+
+def scrapyImg(link):
     time.sleep(0.5)
     re = requests.get(url = link)
     soup = BeautifulSoup(re.text, 'html.parser')
     getImageHead = soup.find("h1",{'class','head'})
-    headTitle = getImageHead.text.strip()[0:256].encode('ISO-8859-1').decode('big5')
+    #print(getImageHead)
+    headTitle = getImageHead.text.strip()[0:256].encode('ISO-8859-1','ignore').decode('big5')
+    #print(headTitle)
     getImageLink = soup.findAll("img",{'class','imglimit'})
+    src = ''
     for Imgsrc in getImageLink:
-        if u'scontent-tpe1-1' in Imgsrc['src'] or u'imgur' in Imgsrc['src']:
-            try:
-                if not os.path.isdir(headTitle):
-                    os.mkdir(headTitle)
-                fname = Imgsrc['src'].split('/')[-1]
-                urllib.request.urlretrieve(Imgsrc['src'], os.path.join(headTitle, fname))
-            except Exception as e:
-                print(e)
-
-def print_pic_json(link):
-    time.sleep(0.5)
-    re = requests.get(url = link)
-    soup = BeautifulSoup(re.text, 'html.parser')
-    articles = []
-    getImageHead = soup.find("h1",{'class','head'})    
-    headTitle = getImageHead.text.strip()[0:256].encode('ISO-8859-1').decode('big5')
-    print(headTitle)
-    getImageLink = soup.findAll("img",{'class','imglimit'})
-    for Imgsrc in getImageLink:
-        # print(Imgsrc)
-        if u'scontent-tpe1-1' in Imgsrc['src'] or u'imgur' in Imgsrc['src']:
-            try:
-                    
-                    fname = Imgsrc['src']+',' 
-
-
-                    json_link = {
-                        'title':headTitle,
-                        'pic':fname
-                    };
-                    # print(json_link);
-                    with open('data.json', 'w', encoding='utf-8') as f:
-                        json.dump(json_link, f, indent=2, sort_keys=True, ensure_ascii=False)
-    
-
-
-            except Exception as e:
-                print(e)
+        if u'scontent-tpe1-1' in Imgsrc['src'] or u'imgur' in Imgsrc['src'] or u'instagram' in Imgsrc['src']:
+            src+= Imgsrc['src']+','
+    jsonStr = {
+        'Title':headTitle,
+        'Imgsrc':src[:-1]
+    }
+    return jsonStr
 
 if __name__ == '__main__':
-    # articles = []
+    PicArray = []
     content = srapy(TIMLIAO_URL)
     IndexArray = get_link(content)
-    # print(IndexArray)
-
     for link in IndexArray:
-       
-        # articles = 
-        # srapy(url)
-        # download_pic(link) #下載圖片
-        print_pic_json(link) #圖片網址
+        return_json = scrapyImg(link)
+        PicArray.append(return_json)
+    print('########## JSON ############## \n',PicArray[0])
+
 
 
 
